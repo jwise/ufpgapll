96e184f0d989de680e08e10fd976eb35ed5400da
Joshua Wise <joshua@joshuawise.com>
2015-01-01 23:37:29 -0800

    fix simulator timing

--- tb.cpp
+++ tb.cpp
@@ -26,35 +26,36 @@ int main(int argc, char **argv, char **env) {
 	atexit(_close_trace);
 #endif
 
-        tb->btns = 0;
-        tb->sw = 0;
-        tb->xtal = 0;
-        tb->fb_u = 0;
+	tb->btns = 0;
+	tb->sw = 0;
+	tb->xtal = 0;
+	tb->fb_u = 0;
 
 #define XTAL 50000000
 
 #if VM_TRACE
-#define TRACE tfp->dump((1e9 / XTAL) * time)
+#define TRACE tfp->dump((1e9 / (XTAL * 2)) * time)
 #else
 #define TRACE
 #endif
 
-#define FREQ 140000
-#define CYC (XTAL / FREQ)
+#define FREQ 150000
+#define CYC (XTAL / (FREQ * 2))
 
-        while (time < 100000) {
-                tb->xtal = 0;
-                if (time % CYC == 0)
-                        tb->fb_u = !tb->fb_u;
-                tb->eval();
-                TRACE;
-                time++;
-                
-                tb->xtal = 1;
-                tb->eval();
-                TRACE;
-                time++;
-        }
+	while (time < 1000000) {
+		tb->xtal = 0;
+
+		tb->eval();
+		TRACE;
+		time++;
+		
+		tb->xtal = 1;
+		if (((time / 2) % CYC) == 0)
+			tb->fb_u = !tb->fb_u;
+		tb->eval();
+		TRACE;
+		time++;
+	}
 	
 	return 0;
 }
